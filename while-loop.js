2a5b90aad6e365d440881ed689df322d7f796440
hadirch <hadirich3@gmail.com>
2023-09-12 21:56:31 +0700

    while-loop

---
+++ while-loop.js
@@ -0,0 +1,11 @@
+/*
+while loop adalah versi perulangan yang lebih sederhana dibanding for loop
+di while loop hanya terdapat kokndisi perulangan, tanpa ada init statement dan post statement
+*/
+let counter = 1;
+while (counter <= 10) {
+    console.log(counter);
+    counter++;
+}
+
+
