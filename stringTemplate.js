5364d30d8fbbd07b5f0e635f0f6ef725f3862692
hadirch <hadirich3@gmail.com>
2023-09-12 21:55:49 +0700

    string-template

---
+++ stringTemplate.js
@@ -0,0 +1,11 @@
+const nama = "Hadi";
+const firstName = "aaaa";
+const middleName = "bbbbb";
+const lastName = "cccccc";
+
+const fullName = `Nama : ${firstName} ${middleName} ${lastName}`
+console.log(fullName);
+
+const value = 80;
+const template = `Name : ${nama}, Lulus : ${value > 75}`;
+console.log(template);
