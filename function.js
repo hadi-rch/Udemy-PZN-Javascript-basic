7751845e83d935a4bbeb80d9ca5da08a7bba57bb
hadirch <hadirich3@gmail.com>
2023-09-12 21:52:41 +0700

    function

---
+++ function.js
@@ -0,0 +1,15 @@
+/*
+function adalah blok kode program yang aakan berjalan saat kita panggil
+sebelumnya kita sudah menggunakan function writeln() unyuk menampilkan tulisan di html
+untuk sebuah function, kita bisa mengguanakan kata kunci function, lalu diikuti dengan nama function, 
+kurung() dan diakhiri dengan blok kurung{}
+kita bisa memanggil function dengan menggunakan nama function lalu diikuti dengan kurung()
+di bahasa lain function juga disebut method
+*/
+//membuat function
+function sayHelloWorld() {
+    console.log("Hello world!");
+}
+//memanggil function
+sayHelloWorld();
+sayHelloWorld();
