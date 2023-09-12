82b74f07656ffc19443029ad09252f39ccc86135
hadirch <hadirich3@gmail.com>
2023-09-12 21:51:36 +0700

    for

---
+++ for.js
@@ -0,0 +1,26 @@
+/*
+Sintak perulangan for
+for(init statement; kondisi; posts statement){
+    //bock perulangan
+}
+-init statement akan dieksekusi hanya sekali di awal sebelum perulangan
+-kodisi akan dilakukan pengecekan dalam setiap perulangan, jika true perulangan akan dilakukan
+ jika false perulangan akan berhenti
+-post statement akan dieksekusi setiap kali diakhiri perulangan
+-inti statement, kondisi dan post statement tidak wajibi diisi jika kondisi tidak diisi berarti
+ kondisi selalu bernilai true
+*/
+// let counter =1;
+// for (;counter <= 10;) {
+//     console.log(counter);
+//     counter++;
+// }
+
+// for (let counter = 1; counter <= 10;) {
+//     console.log(counter);
+//     counter++;
+// }
+
+for (let counter = 1; counter <= 10;  counter++) {
+    console.log(counter);
+}
