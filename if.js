dc3af685ea6de0f403a63502791715f4e716e6e0
hadirch <hadirich3@gmail.com>
2023-09-12 21:53:15 +0700

    if

---
+++ if.js
@@ -0,0 +1,9 @@
+const examValue = 79;
+
+if (examValue > 80) {
+    console.log("good Job")
+} else if (examValue > 60){
+    console.log("lumayan lah")
+} else {
+    console.log("Maybe Next Time")
+}
