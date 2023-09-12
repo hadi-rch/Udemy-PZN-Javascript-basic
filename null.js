cd2ff590d7a5435a83a6bd64f50ac5c9c1ed1f1b
hadirch <hadirich3@gmail.com>
2023-09-12 21:53:56 +0700

    null

---
+++ null.js
@@ -0,0 +1,10 @@
+//null dan undefined
+let firstName = null;
+
+if (firstName === undefined) {
+    console.log("hasil undefined");
+} else if (firstName === null) {
+    console.log("hasil null");
+} else {
+    console.log(` Hello ${firstName}`);
+}
